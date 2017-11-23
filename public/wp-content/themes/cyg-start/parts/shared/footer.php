1207dca51f2e602a51e4a2528b91a1ae37793e83
Benjamin Locher <blocher@gmail.com>
2017-11-22 21:51:11 -0500

    another http error

--- public/wp-content/themes/cyg-start/parts/shared/footer.php
+++ public/wp-content/themes/cyg-start/parts/shared/footer.php
@@ -2,25 +2,25 @@
 				<footer>
 					<div class="row">
 						<div class="col-md-3 hidden-sm hidden-xs">
-						
-							<a href="http://www.guidestar.org/organizations/26-2869322/society-college.aspx" target="_blank">
-							    <img src="http://widgets.guidestar.org/gximage2?o=8653967&l=v4" />
+
+							<a href="https://www.guidestar.org/organizations/26-2869322/society-college.aspx" target="_blank">
+							    <img src="https://widgets.guidestar.org/gximage2?o=8653967&l=v4" />
 							</a>
-							
+
 						</div><!-- ./col-md-3 hidden-sm hidden-xs -->
 						<div class="col-xs-12 col-sm-12 col-md-6">
-					
+
 							<p>&copy; <?php echo date('Y'); ?> The Society for the College<br />
 							P.O. Box 876<br />
 							Kilmarnock, VA 22482</p>
-							
+
 							<?php wp_nav_menu( array(
 									'menu' => 'Footer Menu',
 									'container' => ''
 								) ) ; ?>
 						</div><!-- /.col-xs-12 col-sm-12 col-md-6 -->
 						<div class="col-md-3 hidden-sm hidden-xs">
-						
+
 						</div><!-- /.col-md-3 hidden-sm hidden-xs -->
 				</footer>
 			</div><!-- /.page -->
